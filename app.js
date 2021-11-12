5877f859aa90f0393958ab5a013f0d03f98900ab
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 00:55:32 +0000

    app

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const Discord = require("djs-self");
 const Chalk = require("chalk");
 const config = require("./config/config.json")
-const client = new Discord.Client();
-require('./utils/eventloader.js')(client);
 
-client.login(config.token)
+
+const app = (() => {
+	const client = new Discord.Client();
+	require('./utils/eventloader.js')(client);
+
+	client.login(config.token)
+})
+
+module.exports = {
+	app: app
+}
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
 const Chalk = require("chalk");
 const { install } = require("./install.js")
-const { discocli } = require("./app.js")
+const { app } = require("./app.js")
+
+const not_installed_message = "Installation has not occurred yet. Commencing the installation process.";
 
 if (!fs.existsSync("./config/.installed")) {
-	console.log("Installation has not occurred yet. Commencing the installation process.")
+	console.log(not_installed_message)
 	install();
 }
+app();
