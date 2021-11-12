72a2add24c9555beb8db8aca26cae9e578d19303
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 00:34:31 +0000

    install func

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const fs = require("fs");
-
+const Chalk = require("chalk");
+const { install } = require("./install.js")
+if (!fs.existsSync("./config/install.json")) {
+	console.log("Installation has not occurred yet. Commencing the installation process.")
+	install();
+}
