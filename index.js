57cc6d15eee8d32674919ef50c4ea31b4df33642
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 00:40:58 +0000

    discocli init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const Chalk = require("chalk");
 const { install } = require("./install.js")
+const { discocli } = require("./app.js")
 
 if (!fs.existsSync("./config/.installed")) {
 	console.log("Installation has not occurred yet. Commencing the installation process.")
