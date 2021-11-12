5db761f9f38240582b79bf63099aaba7b4c4dab9
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 01:09:09 +0000

    fixed undefined#undefined

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Chalk = require('chalk');
 const Discord = require("djs-self");
 
 module.exports = async (client) => {
-    Chalk.green(console.log("Logged in as " + client.username + "#" + client.discriminator + "!"))
+    Chalk.green(console.log("Logged in as " + client.user.username + "#" + client.user.discriminator + "!"))
 };
